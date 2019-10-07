ed949fcea3c5d0557397ef95c4df417acf0d906d
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2019-10-07 13:23:46 -0700

    Use new spreadsheet format

--- src/main.js
+++ src/main.js
@@ -3,13 +3,12 @@ import "~/styles";
 
 import { sanitize } from "dompurify";
 import marked from "marked";
-import "~/tab";
+import enableTabs from "~/tab";
 
 const sheetId = "1k9WZcsauAp6LI7T_eIzQ_tr2WgWo86pYUMTtkl0CH3c"; // this is the id of the spreadsheet
 const url = `https://spreadsheets.google.com/feeds/list/${sheetId}/od6/public/values?alt=json`;
 
-const directions = document.querySelector("#directions"); // get div element with id "directions"
-const questions = document.querySelector("#questions"); // get div element with id "questions"
+const parent = document.querySelector("#parent"); // get element with id "parent"
 
 // either "dark" or "light"
 const theme = "dark";
@@ -23,42 +22,62 @@ if (navbar) {
   navbar.classList.add(`navbar-${theme}`, `bg-${theme}`);
 }
 
+let entries = [];
+
 async function getData() {
   const data = await fetch(url).then(d => d.json()); // send GET request to fetch data
   if (!data) return []; // return empty array if no data
 
-  const logs = [...data.feed.entry]
-    .map(e => ({
-      date: new Date(e.gsx$date.$t),
-      directions: marked(sanitize(e.gsx$directions.$t)),
-      questions: String(e.gsx$questions.$t).split("\n")
-    })) // gather data from sheet and arrange into interface
-    .sort((a, b) => b.date - a.date); // sort by date (newest first)
+  entries = [...data.feed.entry].map(e => ({
+    title: e.gsx$title.$t,
+    prompt: marked(sanitize(e.gsx$prompt.$t))
+  }));
 
-  return logs;
+  return entries;
 }
 
 async function start() {
-  if (!directions || !questions) return []; // return if can't find divs
+  if (!parent) return; // return if can't find divs
 
-  const logs = await getData();
+  await getData();
 
-  const log =
-    logs.filter(l => l.date.toDateString() === new Date().toDateString())[0] ||
-    logs[0]; // get the log for today (and if it doesn't exist, get the most recent log)
-
-  if (!log) {
-    directions.innerHTML = "No directions available today";
-    questions.innerHTML = "No questions available today";
+  if (!entries.length) {
     return;
   }
 
-  directions.innerHTML = log.directions; // show directions
+  entries.forEach(e => {
+    const el = document.createElement("div");
+    el.className = "row mb-4";
+    el.id = `prompt-${entries.indexOf(e)}`;
+    el.innerHTML = `<div class="col-sm">
+      <h3>${e.title}</h3>
+      <p>${e.prompt}</p>
+    </div>
+    <div class="col-sm">
+      <textarea
+        class="form-control textarea"
+        rows="10"
+      ></textarea>
+    </div>`;
 
-  questions.innerHTML = `
-  <ol>
-  ${log.questions.map(q => `<li>${q}</li>`).join("\n")}
-  </ol>`; // show questions
+    parent.appendChild(el);
+  });
+
+  enableTabs();
 }
+
+window.create = () => {
+  const answers = entries.map(e => {
+    const textarea = document.querySelector(
+      `#prompt-${entries.indexOf(e)} textarea`
+    );
+
+    if (!textarea) return;
+
+    return textarea.value;
+  });
+
+  console.log(answers);
+};
 
 start();
