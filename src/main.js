d84b5fa3f5a612f9fbba8b82a654f21226b4fd6a
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2019-10-20 13:08:43 -0700

    Complete js portion

--- src/main.js
+++ src/main.js
@@ -5,9 +5,10 @@ import { sanitize } from "dompurify";
 import marked from "marked";
 import enableTabs from "~/tab";
 import axios from "axios";
+import "~/gapi";
 
 const sheetId = "1k9WZcsauAp6LI7T_eIzQ_tr2WgWo86pYUMTtkl0CH3c";
-const url = `https://spreadsheets.google.com/feeds/list/${sheetId}/od6/public/values?alt=json`;
+const url = `https://spreadsheets.google.com/feeds/list/${sheetId}/1/public/values?alt=json`;
 
 const parent = document.querySelector("#parent");
 
@@ -89,7 +90,47 @@ window.create = async () => {
     return textarea.value;
   });
 
-  console.log(answers);
+  if (!gapi.auth2.getAuthInstance().isSignedIn.get()) {
+    return signIn();
+  }
+
+  const date = new Date().toLocaleDateString();
+
+  const res = await gapi.client.docs.documents.create({
+    title: `Daily Log - ${date}`
+  });
+
+  const req = {
+    requests: [
+      {
+        insertText: {
+          text: `Daily Log for ${date}\n\n${answers.join("\n\n")}`,
+          location: {
+            index: 1,
+            segmentId: ""
+          }
+        }
+      }
+    ]
+  };
+
+  await gapi.client.docs.documents.batchUpdate(
+    { documentId: res.result.documentId },
+    req
+  );
 };
+
+window.signIn = () => gapi.auth2.getAuthInstance().signIn();
+window.signOut = () => gapi.auth2.getAuthInstance().signOut();
+
+gapi.load("client:auth2", async () => {
+  await gapi.client.init({
+    apiKey: "AIzaSyADwwNFoJFdhY53K8vsJQKNHTCxGwsiiHU",
+    clientId:
+      "438020323125-jpjei4hnp58fi80sqseg70frdjdil51h.apps.googleusercontent.com",
+    discoveryDocs: ["https://docs.googleapis.com/$discovery/rest?version=v1"],
+    scope: "https://www.googleapis.com/auth/drive.file"
+  });
+});
 
 start();
