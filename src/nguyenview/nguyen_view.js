ba717ec01b7f4b05d75318d41fe51a76c2323029
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2021-02-24 13:29:45 -0800

    commit so that marissa can test the code (not finished)

--- src/nguyenview/nguyen_view.js
+++ src/nguyenview/nguyen_view.js
@@ -1,4 +1,3 @@
-
 const config = {
     apiKey: "AIzaSyADwwNFoJFdhY53K8vsJQKNHTCxGwsiiHU",
     authDomain: "daily-log-creator.firebaseapp.com",
@@ -8,56 +7,47 @@ const config = {
     messagingSenderId: "438020323125",
     appId: "1:438020323125:web:2c78e3f40c689c1d6f3e7c"
 };
-
-
-
 firebase.initializeApp(config);
 let db = firebase.firestore();
-let users = [];
-
-
-db.collection("users").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id}`);
-        users.push(doc.id);
-    });
-});
-
-console.log(users);
-
-
 
 //function getTotalUsers() {
-
 //}
 
-//function getName() {
-
-//}
+function getNames() {
+    let users = [];
+    db.collection("users").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            users.push(doc.id);
+        });
+    });
+    return users
+}
 
 //function getID() {
-
 //}
 
 //function getPeriod() {
-
 //}
 
 
 
 //adds total number of rows according to number of students
-function addRows() {
-	var user0 = users[0]; //is currently undefined
-	
+function addRow(name) {	
 	var table = document.getElementById("table");
 	var row = table.insertRow(1);
-	var cell1 = row.insertCell(0);
-  	var cell2 = row.insertCell(1);
-  	var cell3 = row.insertCell(2);
-  	cell1.innerHTML = "getName()";
-  	cell2.innerHTML = "getID()";
-  	cell3.innerHTML = "getPeriod()";
+	var name_row = row.insertCell(0);
+  	var id_row = row.insertCell(1);
+  	var period_row = row.insertCell(2);
+
+  	name_row.innerHTML = name;
+  	id_row.innerHTML = "getID()";
+  	period_row.innerHTML = "getPeriod()";
 }
 
-
-addRows();
+const names = getNames()
+console.log(names)
+console.log(names.length)
+for (i=0; i<names.length; i++) {
+    addRow(names[i]);
+    console.log(names[i])
+}
