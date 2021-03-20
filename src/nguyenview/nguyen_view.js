34d5bb76a8df4a3db12b7648339edb2b1a48b750
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2021-03-20 10:26:26 -0700

    add code to insert rows with name period and email, sorts name in alphabetical order

--- src/nguyenview/nguyen_view.js
+++ src/nguyenview/nguyen_view.js
@@ -8,46 +8,42 @@ const config = {
     appId: "1:438020323125:web:2c78e3f40c689c1d6f3e7c"
 };
 firebase.initializeApp(config);
-let db = firebase.firestore();
+const db = firebase.firestore();
+const users = db.collection("users")
 
-//function getTotalUsers() {
-//}
-
-function getNames() {
-    let users = [];
-    db.collection("users").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            users.push(doc.id);
-        });
+async function getDict() {
+    let dict = {};
+    await users.get().then(querySnapshot => {
+        querySnapshot.forEach(doc => dict[doc.id] = doc.data());
     });
-    return users
+    return dict
 }
 
-//function getID() {
-//}
-
-//function getPeriod() {
-//}
-
-
-
-//adds total number of rows according to number of students
-function addRow(name) {	
+function addRow(value) {
 	var table = document.getElementById("table");
 	var row = table.insertRow(1);
-	var name_row = row.insertCell(0);
-  	var id_row = row.insertCell(1);
-  	var period_row = row.insertCell(2);
+	var name_col = row.insertCell(0);
+  	var id_col = row.insertCell(1);
+  	var period_col = row.insertCell(2);
 
-  	name_row.innerHTML = name;
-  	id_row.innerHTML = "getID()";
-  	period_row.innerHTML = "getPeriod()";
+  	name_col.innerHTML = value[0]["name"];
+  	id_col.innerHTML = value[0]["email"];
+  	period_col.innerHTML = value[0]["period"];
 }
 
-const names = getNames()
-console.log(names)
-console.log(names.length)
-for (i=0; i<names.length; i++) {
-    addRow(names[i]);
-    console.log(names[i])
+async function initialize() {
+    const user_dict = await getDict();
+    const name_dict = {};
+    const names = [];
+
+    for (const [key, value] of Object.entries(user_dict)) {
+        name_dict[value["name"]] = [value];
+        names.push(value["name"]);
+    } names.reverse();
+
+    for (let i=0; i<names.length; i++) {
+        console.log(names[i])
+        addRow(name_dict[names[i]]);
+    }
 }
+initialize();
